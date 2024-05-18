a31d258fe2b55ef5b9117857a65de21a9f05e559
BHARAT BHATT <s223148345@deakin.edu.au>
2024-05-18 23:00:20 +1000

    Final

--- src/test/java/sit707_week2/MainTest.java
+++ src/test/java/sit707_week2/MainTest.java
@@ -105,7 +105,7 @@ public class MainTest {
         loginButton.click();
         Thread.sleep(3000);
 
-        WebElement errorMessage = driver.findElement(By.xpath("//*[@id=\"retailLogin\"]/div[1]/div[1]/div/div/p")); // Update with correct locator
+        WebElement errorMessage = driver.findElement(By.xpath("//*[@id=\"retailLogin\"]/div[1]/div[1]/div/div/p")); 
         Assert.assertTrue(errorMessage.isDisplayed());
     }
     
